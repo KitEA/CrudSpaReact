a23770b8b7098eebb6a5cc34b685c3289ed75e78
Kit_EA <kit.earf@gmail.com>
2018-12-23 14:21:58 +0500

    Css and Bootstrap

--- src/Main.js
+++ src/Main.js
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
 import {
-  Route,
-  NavLink,
-  HashRouter
+    Route,
+    NavLink,
+    HashRouter
 } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from "./components/Home";
 import Stuff from "./components/Stuff";
 import Contact from "./components/Contact";
@@ -12,13 +13,23 @@ class Main extends Component {
     render() {
         return (
             <HashRouter>
-                <div>
-                    <h1>Simple SPA</h1>
-                    <ul className="header">
-                        <li><NavLink to="/">Home</NavLink></li>
-                        <li><NavLink to="/stuff">Stuff</NavLink></li>
-                        <li><NavLink to="/contact">Contact</NavLink></li>
-                    </ul>
+                <div className="container">
+                    <h1>Workers Listing</h1>
+                    <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
+                        <div className="collapse navbar-collapse">
+                            <ul className="navbar-nav mr-auto">
+                                <li className="nav-item">
+                                    <NavLink exact to="/" className="nav-link">Home</NavLink>
+                                </li>
+                                <li className="nav-item">
+                                    <NavLink to="/stuff" className="nav-link">Stuff</NavLink>
+                                </li>
+                                <li className="nav-item">
+                                    <NavLink to="/contact" className="nav-link">Contact</NavLink>
+                                </li>
+                            </ul>
+                        </div>
+                    </nav>
                     <div className="content">
                         <Route exact path="/" component={Home}/>
                         <Route path="/stuff" component={Stuff}/>
